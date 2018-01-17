42234b668841df834f53bf12c19d0ac4f52ff251
Nicholas Rodine <halfofastaple@gmail.com>
2018-01-17 17:56:54 -0500

    Started fleshing out a pattern for parsing sql.

--- QueryInspector.Test/QueryFactories/SelectQueryFactoryTests.cs
+++ QueryInspector.Test/QueryFactories/SelectQueryFactoryTests.cs
@@ -10,5 +10,13 @@ namespace QueryInspector.Test.QueryFactories {
 			var factory = new SelectQueryFactory();
 			factory.CanParseQuery("select 1").Should().BeTrue();
 		}
+
+		[Test]
+		public void CorrectlyIdentifiesTable() {
+			var factory = new SelectQueryFactory();
+			var query = factory.ParseQuery("select * from users");
+
+			query.Should().Be("users");
+		}
 	}
 }

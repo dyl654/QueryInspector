2af78bb8ecb88667eae1e0f1776d819aae67a7c6
Nicholas Rodine <halfofastaple@gmail.com>
2018-11-18 14:09:37 -0500

    Got tests working by adjusting regex.

--- QueryInspector/QueryFactories/SelectQueryFactory.cs
+++ QueryInspector/QueryFactories/SelectQueryFactory.cs
@@ -4,26 +4,29 @@ using QueryInspector.Models;
 
 namespace QueryInspector.QueryFactories {
 	public class SelectQueryFactory : IQueryFactory {
-		private readonly Regex _selectStatement = new Regex(@"^select(.*)from(.*)$");
+		private readonly Regex _selectStatement = new Regex(@"^select(.*?)(from(.*))?$");
+		
 		public bool CanParseQuery(string sql) {
 			return _selectStatement.IsMatch(sql);
 		}
 
 		public ISelectQuery ParseQuery(string sql) {
 			var match = _selectStatement.Match(sql);
-			var table = match.Groups[2].Value;
-			var columns = GetColumns(match.Groups[1].Value);
 			return new SelectQuery {
-				Columns = columns,
-				Table = new Table {
-					Name = table
-				}
+				Columns = GetColumns(match.Groups[1].Value),
+				Table = GetTable(match.Groups[3].Value)
+			};
+		}
+
+		private static Table GetTable(string table) {
+			return new Table {
+				Name = table.Trim()
 			};
 		}
 
 		protected virtual IEnumerable<IColumn> GetColumns(string columns) {
 			yield return new Column {
-				Name = columns
+				Name = columns.Trim()
 			};
 		}
 	}
